4eced14d7b8ee1988cb30c06d49de701f436752f
DESKTOP-IJLO8PN\Admin <nguyenhoangbaoha10a1@gmail.com>
2025-08-12 23:46:30 +0700

    Update Model

--- backend/UserRoleManagementAPI/UserRoleManagementAPI/Models/Role.cs
+++ backend/UserRoleManagementAPI/UserRoleManagementAPI/Models/Role.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string RoleName { get; set; } = null;
         public string? Decription { get; set; }
+        public ICollection<User> Users { get; set; } = new List<User>();
     }
 }
---
+++ backend/UserRoleManagementAPI/UserRoleManagementAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace UserRoleManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public int RoleId { get; set; }
+        public Role Role { get; set; } = null!;
+        public DateTime CreateAt { get; set; }
+        public byte Status { get; set; }
+        public byte[]? RowVersion { get; set; }
+    }
+}
